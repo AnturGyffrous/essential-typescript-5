b299fd598d7e8f38982e355b2b10a0cc884012b2
Owain Williams <owain@hedfan.uk>
2025-08-14 22:00:13 +0100

    Compile the todo project using TypeScript compiler

--- chapter-02/todo/dist/index.js
+++ chapter-02/todo/dist/index.js
@@ -8,9 +8,10 @@ let todos = [
 ];
 let collection = new todoCollection_1.TodoCollection("Owain", todos);
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
+console.log(`${collection.userName}'s Todo List `
+    + `(${collection.getItemCounts().incomplete} items to do)`);
 // let newId: number = collection.addTodo("Go for run");
 // let todoItem: TodoItem = collection.getTodoById(newId);
 // todoItem.printDetails();
-collection.removeComplete();
+// collection.removeComplete();
 collection.getTodoItems(true).forEach(item => item.printDetails());
