d1ace795b7ae46bed85daa32da5a2b46c696b9a6
Owain Williams <owain@hedfan.uk>
2025-08-17 22:30:57 +0100

    Compile the todo project using TypeScript compiler

--- chapter-02/todo/dist/index.js
+++ chapter-02/todo/dist/index.js
@@ -1,11 +1,11 @@
 import { TodoItem } from "./todoItem.js";
-import { TodoCollection } from "./todoCollection.js";
 import inquirer from "inquirer";
+import { JsonTodoCollection } from "./jsonTodoCollection.js";
 let todos = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)
 ];
-let collection = new TodoCollection("Owain", todos);
+let collection = new JsonTodoCollection("Owain", todos);
 let showCompleted = true;
 function displayTodoList() {
     console.log(`${collection.userName}'s Todo List `
