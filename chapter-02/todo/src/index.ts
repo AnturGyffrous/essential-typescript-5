e04a81d2b94e530c0bb20ac3e992e38cbf0fbf57
Owain Williams <owain@hedfan.uk>
2025-08-17 22:02:22 +0100

    Add an 'Add' command that adds new tasks

--- chapter-02/todo/src/index.ts
+++ chapter-02/todo/src/index.ts
@@ -17,8 +17,20 @@ function displayTodoList(): void {
 }
 
 enum Commands {
+    Add = "Add New Task",
     Toggle = "Show/Hide Completed",
     Quit = "Quit"
+}
+
+function promptAdd(): void {
+    console.clear();
+    inquirer.prompt({ type: "input", name: "add", message: "Enter task:" })
+        .then(answers => {
+            if (answers["add"] !== "") {
+                collection.addTodo(answers["add"]);
+            }
+            promptUser();
+        })
 }
 
 function promptUser(): void {
@@ -34,6 +46,9 @@ function promptUser(): void {
             case Commands.Toggle:
                 showCompleted = !showCompleted
                 promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
                 break;
         }
     })
