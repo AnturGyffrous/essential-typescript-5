db8c0a663763367d215f89b6192576a352b8ccd3
Owain Williams <owain@hedfan.uk>
2025-08-31 22:31:37 +0100

    Replace index.js with objects representing the items

--- chapter-03/primer/index.js
+++ chapter-03/primer/index.js
@@ -1,52 +1,15 @@
-let hatPrice = 100;
-console.log(`Hat price: ${hatPrice}`);
-let bootsPrice = "100";
-console.log(`Boots price: ${bootsPrice}`);
+let hat = {
+    name: "Hat",
+    price: 100
+};
 
-if (hatPrice === bootsPrice) {
-    console.log("Prices are the same");
-} else {
-    console.log("Prices are different");
-}
-
-let names = ["Hat", "Boots", "Gloves"];
-
-let [, , three] = names;
-console.log(`Three: ${three}`);
-
-let prices = [100, 120, 50.25];
-
-let [, ...highest] = prices.sort((a, b) => a - b);
-highest.forEach(price => console.log(`High price: ${price}`));
-
-console.log(`First Item: ${names[0]}: ${prices[0]}`);
+let boots = {
+    name: "Boots",
+    price: "100"
+};
 
 let sumPrices = (...numbers) => numbers.reduce((total, val) =>
     total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
 
-let totalPrice = sumPrices(...prices);
+let totalPrice = sumPrices(hat.price, boots.price);
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-let combinedArray = [...names, ...prices];
-combinedArray.forEach(element =>
-    console.log(`Combined Array Element: ${element}`));
-
-totalPrice = sumPrices(100, 200, 300);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-totalPrice = sumPrices(100, 200, undefined, false, "hello");
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-let myVariable = "Owain";
-console.log(`Type: ${typeof myVariable}`);
-myVariable = 100;
-console.log(`Type: ${typeof myVariable}`);
-
-let firstCity;
-let secondCity = firstCity || "London";
-console.log(`City: ${secondCity}`);
-
-let taxRate; // no tax rate has been defined
-console.log(`Tax rate: ${taxRate ?? 10}%`);
-taxRate = 0; // zero-rated for tax
-console.log(`Tax rate: ${taxRate ?? 10}%`);
