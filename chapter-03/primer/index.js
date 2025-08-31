1b4378715ab74c924917e7e1c829a560a693bbcb
Owain Williams <owain@hedfan.uk>
2025-08-31 23:06:00 +0100

    Add getters and setters to the object properties

--- chapter-03/primer/index.js
+++ chapter-03/primer/index.js
@@ -1,37 +1,31 @@
 let hat = {
     name: "Hat",
-    price: 100
+    _price: 100,
+    priceIncTax: 100 * 1.2,
+
+    set price(newPrice) {
+        this._price = newPrice;
+        this.priceIncTax = this._price * 1.2;
+    },
+
+    get price() {
+        return this._price;
+    }
 };
 
 let boots = {
     name: "Boots",
-    price: "100"
+    price: "100",
+
+    get priceIncTax() {
+        return Number(this.price) * 1.2;
+    }
 };
 
-let additionalProperties = { ...hat, discounted: true };
-console.log(`Additional: ${JSON.stringify(additionalProperties)}`);
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
+hat.price = 120;
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
 
-let replacedProperties = { ...hat, price: 10 };
-console.log(`Replaced: ${JSON.stringify(replacedProperties)}`);
-
-let { price, ...someProperties } = hat;
-console.log(`Selected: ${JSON.stringify(someProperties)}`);
-
-let gloves = {
-    productName: "Gloves",
-    price: "40"
-};
-
-gloves.name = gloves.productName;
-delete gloves.productName;
-gloves.price = 20;
-
-let propertyCheck = hat.price ?? 0;
-let objectAndPropertyCheck = hat?.price ?? 0;
-console.log(`Checks: ${propertyCheck}, ${objectAndPropertyCheck}`);
-
-let sumPrices = (...numbers) => numbers.reduce((total, val) =>
-    total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
-
-let totalPrice = sumPrices(hat.price, boots.price, gloves.price);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
+boots.price = "120";
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
