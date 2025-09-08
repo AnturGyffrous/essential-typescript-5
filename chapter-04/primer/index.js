9adcfb002ff163035c68e54d9b2e40a4234380ee
Owain Williams <owain@hedfan.uk>
2025-09-08 21:59:27 +0100

    Use a constructor function to create new objects

--- chapter-04/primer/index.js
+++ chapter-04/primer/index.js
@@ -1,18 +1,14 @@
-let hat = {
-    name: "Hat",
-    price: 100,
-    getPriceIncTax() {
-        return Number(this.price) * 1.2;
-    }
-};
+let Product = function (name, price) {
+    this.name = name;
+    this.price = price;
+}
 
-let boots = {
-    name: "Boots",
-    price: 100,
-    getPriceIncTax() {
-        return Number(this.price) * 1.2;
-    }
-};
+Product.prototype.toString = function () {
+    return `toString: Name: ${this.name}, Price: ${this.price}`;
+}
+
+let hat = new Product("Hat", 100);
+let boots = new Product("Boots", 100);
 
 let hatPrototype = Object.getPrototypeOf(hat);
 console.log(`Hat Prototype: ${hatPrototype}`);
@@ -21,10 +17,6 @@ let bootsPrototype = Object.getPrototypeOf(boots);
 console.log(`Boots Prototype: ${bootsPrototype}`);
 
 console.log(`Common prototype: ${hatPrototype === bootsPrototype}`);
-
-hatPrototype.toString = function () {
-    return `toString: Name: ${this.name}, Price: ${this.price}`;
-}
 
 console.log(hat.toString());
 console.log(boots.toString());
