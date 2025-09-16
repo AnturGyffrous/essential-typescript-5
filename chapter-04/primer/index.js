2d51af029d312e36ddad3a15eec02aafc16ef241
Owain Williams <owain@hedfan.uk>
2025-09-16 11:45:24 +0100

    Remove constructors and prototypes and introduce a Product class
    JavaScript classes were added to the language to ease the transition
    from other popular programming languages. Behind the scenes, JavaScript
    classes are implemented using prototypes, which means that JavaScript
    classes have some differences from those in languages such as C# and
    Java

--- chapter-04/primer/index.js
+++ chapter-04/primer/index.js
@@ -1,48 +1,16 @@
-let Product = function (name, price) {
-    this.name = name;
-    this.price = price;
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    toString() {
+        return `toString: Name: ${this.name}, Price: ${this.price}`;
+    }
 }
 
-Product.prototype.toString = function () {
-    return `toString: Name: ${this.name}, Price: ${this.price}`;
-}
-
-let TaxedProduct = function (name, price, taxRate) {
-    Product.call(this, name, price);
-    this.taxRate = taxRate;
-}
-
-Object.setPrototypeOf(TaxedProduct.prototype, Product.prototype);
-
-TaxedProduct.prototype.getPriceIncTax = function () {
-    return Number(this.price) * this.taxRate;
-}
-
-TaxedProduct.prototype.toTaxString = function () {
-    return `${this.toString()}, Tax: ${this.getPriceIncTax()}`;
-}
-
-let hat = new TaxedProduct("Hat", 100, 1.2);
+let hat = new Product("Hat", 100);
 let boots = new Product("Boots", 100);
 
-let hatPrototype = Object.getPrototypeOf(hat);
-console.log(`Hat Prototype: ${hatPrototype}`);
-
-let bootsPrototype = Object.getPrototypeOf(boots);
-console.log(`Boots Prototype: ${bootsPrototype}`);
-
-console.log(`Common prototype: ${hatPrototype === bootsPrototype}`);
-
-console.log(hat.toTaxString());
+console.log(hat.toString());
 console.log(boots.toString());
-console.log(`hat and TaxedProduct: ${hat instanceof TaxedProduct}`);
-console.log(`hat and Product: ${hat instanceof Product}`);
-console.log(`boots and TaxedProduct: ${boots instanceof TaxedProduct}`);
-console.log(`boots and Product: ${boots instanceof Product}`);
-
-Product.process = (...products) =>
-    products.forEach(p => p instanceof TaxedProduct
-        ? console.log(p.toTaxString())
-        : console.log(p.toString()));
-
-Product.process(new TaxedProduct("Hat", 100, 1.2), new Product("Boots", 100));
