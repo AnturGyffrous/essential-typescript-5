4545b4920ba77eb518839ee858583b81ad2ca4ae
Owain Williams <owain@hedfan.uk>
2025-09-17 12:09:37 +0100

    Define and use an iterator

--- chapter-04/primer/index.js
+++ chapter-04/primer/index.js
@@ -36,3 +36,38 @@ class TaxedProduct extends Product {
 TaxedProduct.process(
     new TaxedProduct("Hat", 100),
     new TaxedProduct("Boots", 100, 1.3));
+
+function createProductIterator() {
+    const hat = new Product("Hat", 100);
+    const boots = new Product("Boots", 100);
+    const umbrella = new Product("Umbrella", 23);
+
+    let lastVal;
+
+    return {
+        next() {
+            switch (lastVal) {
+                case undefined:
+                    lastVal = hat;
+                    return { value: hat, done: false };
+                case hat:
+                    lastVal = boots;
+                    return { value: boots, done: false };
+                case boots:
+                    lastVal = umbrella;
+                    return { value: umbrella, done: false };
+                case umbrella:
+                    return {
+                        value: undefined, done: true
+                    }
+            }
+        }
+    }
+}
+
+let iterator = createProductIterator();
+let result = iterator.next();
+while (!result.done) {
+    console.log(result.value.toString());
+    result = iterator.next();
+}
