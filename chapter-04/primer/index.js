add04d1739973988f19930dd78a9ee27ffa9997f
Owain Williams <owain@hedfan.uk>
2025-09-25 15:49:31 +0100

    Define multiple named features in a utils.js module

--- chapter-04/primer/index.js
+++ chapter-04/primer/index.js
@@ -1,4 +1,5 @@
 import calcTaxAndSum, { calculateTax } from "./tax.js"
+import { printDetails, applyDiscount } from "./utils.js";
 
 class Product {
     constructor(name, price) {
@@ -93,3 +94,6 @@ console.log(`Name: ${product.name}, Taxed Price: ${taxedPrice}`);
 let products = [new Product("Gloves", 23), new Product("Boots", 100)];
 let totalPrice = calcTaxAndSum(...products.map(p => p.price));
 console.log(`Total Price: ${totalPrice.toFixed(2)}`);
+
+applyDiscount(product, 10);
+printDetails(product);
