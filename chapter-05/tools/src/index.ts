401c901a87d23cdba4aecca6469811b818580a4b
Owain Williams <owain@hedfan.uk>
2025-09-26 13:58:59 +0100

    Use a Map which was only added to JavaScript in es2015
    src/index.ts(6,16): error TS2583: Cannot find name 'Map'. Do you need to
    change your target library? Try changing the 'lib' compiler option to
    'es2015' or later.

--- chapter-05/tools/src/index.ts
+++ chapter-05/tools/src/index.ts
@@ -2,3 +2,8 @@ let printMessage = (msg: string)
     : void => console.log(`Message: ${msg}`);
 
 printMessage("Hello, TypeScript");
+
+let data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach((val, key) => console.log(`${key} lives in ${val}`));
