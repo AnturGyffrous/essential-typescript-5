fc6ad55e41d1d0754327f6fbb3d9d9a01d2afe59
Owain Williams <owain@hedfan.uk>
2025-09-30 14:43:05 +0100

    Define type annotations to variables so they use static types too

--- chapter-07/types/dist/index.js
+++ chapter-07/types/dist/index.js
@@ -10,6 +10,8 @@ console.log();
 function calculateTax(amount) {
     return amount * 1.2;
 }
-console.log(`${12} = ${calculateTax(12)}`);
-console.log(`${"Hello"} = ${calculateTax("Hello")}`);
-console.log(`${true} = ${calculateTax(true)}`);
+let price = 100;
+let taxAmount = calculateTax(price);
+let halfShare = taxAmount / 2;
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half share: ${halfShare}`);
