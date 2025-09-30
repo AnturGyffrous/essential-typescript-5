583c130e9cfd5ec04fa6029086ea0b382c5d3400
Owain Williams <owain@hedfan.uk>
2025-09-30 14:20:50 +0100

    Define a function with a dynamic parameter type
    Function parameter types are also dynamic, which means that the
    calculateTax function may receive values of any type. The statements
    that follow the function invoke it with number, string, and boolean
    values

--- chapter-07/types/src/index.ts
+++ chapter-07/types/src/index.ts
@@ -6,3 +6,13 @@ myVar = "Hello";
 console.log(`${myVar} = ${typeof myVar}`);
 myVar = true;
 console.log(`${myVar} = ${typeof myVar}`);
+
+console.log();
+
+function calculateTax(amount) {
+    return amount * 1.2;
+}
+
+console.log(`${12} = ${calculateTax(12)}`);
+console.log(`${"Hello"} = ${calculateTax("Hello")}`);
+console.log(`${true} = ${calculateTax(true)}`);
