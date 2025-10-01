23887e4785cb3ebefb34e675ce0d13c910b640ba
Owain Williams <owain@hedfan.uk>
2025-10-01 10:53:44 +0100

    Use a type union in the calculateTax function
    The type returned by the calculateTax function is the union of the
    string and number types, which is defined using the bar character
    between type names, you can combine as many types as you need to create
    a union.

--- chapter-07/types/src/index.ts
+++ chapter-07/types/src/index.ts
@@ -9,16 +9,20 @@ console.log(`${myVar} = ${typeof myVar}`);
 
 console.log();
 
-function calculateTax(amount): any {
-    return `$${(amount * 1.2).toFixed(2)}`;
+function calculateTax(amount: number, format: boolean): string | number {
+    const calcAmount = amount * 1.2;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
 
-let price = 100;
-let taxAmount = calculateTax(price);
-let halfShare = taxAmount / 2;
+// let price = 100;
+// let taxAmount = calculateTax(price, false);
+// let halfShare = taxAmount / 2;
 
-let personVal = calculateTax("Bob");
+// let personVal = calculateTax("Bob");
 
-console.log(`Full amount in tax: ${taxAmount}`);
-console.log(`Half share: ${halfShare}`);
-console.log(`Name: ${personVal}`);
+// console.log(`Full amount in tax: ${taxAmount}`);
+// console.log(`Half share: ${halfShare}`);
+// console.log(`Name: ${personVal}`);
+
+let taxNumber = calculateTax(100, false);
+let taxString = calculateTax(100, true);
