c0618afb0d72a0d0126be3ef2d7c23827d38e33a
Owain Williams <owain@hedfan.uk>
2025-10-10 17:23:48 +0100

    Use a type union and check for null

--- chapter-08/types/dist/index.js
+++ chapter-08/types/dist/index.js
@@ -1,5 +1,7 @@
 function calculateTax(amount, discount = 0, ...extraFees) {
-    return (amount * 1.2) - discount + extraFees.reduce((total, val) => total + val, 0);
+    if (amount != null) {
+        return (amount * 1.2) - discount + extraFees.reduce((total, val) => total + val, 0);
+    }
 }
 let taxValue = calculateTax(null, 0);
 console.log(`Tax value: ${taxValue}`);
--- chapter-08/types/src/index.ts
+++ chapter-08/types/src/index.ts
@@ -1,5 +1,7 @@
-function calculateTax(amount: number, discount: number = 0, ...extraFees: number[]) {
-    return (amount * 1.2) - discount + extraFees.reduce((total, val) => total + val, 0);
+function calculateTax(amount: number | null, discount: number = 0, ...extraFees: number[]) {
+    if (amount != null) {
+        return (amount * 1.2) - discount + extraFees.reduce((total, val) => total + val, 0);
+    }
 }
 
 let taxValue = calculateTax(null, 0);
