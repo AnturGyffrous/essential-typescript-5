4fc0e9fce717fa56f8696b02406aa21515841d20
Owain Williams <owain@hedfan.uk>
2025-10-13 16:35:36 +0100

    Use tuples with rest elements
    Tuples can also contain a rest element, that can be used to match
    multiple values of a given type. This feature produces a variable-length
    tuple that lacks the rigidly defined structure of basic tuples.

--- chapter-09/types/dist/index.d.ts
+++ chapter-09/types/dist/index.d.ts
@@ -1,6 +1,6 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
-declare let hat: [string, number, number?];
-declare let gloves: [string, number, number?];
-declare let products: [string, number, number?][];
-declare let tupleUnion: ([string, number, number?] | boolean)[];
+declare let hat: [string, number, number?, ...number[]];
+declare let gloves: [string, number, number?, ...number[]];
+declare let products: [string, number, number?, ...number[]][];
+declare let tupleUnion: ([string, number, number?, ...number[]] | boolean)[];
--- chapter-09/types/src/index.ts
+++ chapter-09/types/src/index.ts
@@ -6,21 +6,22 @@ function writePrice(product: string, price: number): void {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-let hat: [string, number, number?] = ["Hat", 100];
-let gloves: [string, number, number?] = ["Gloves", 75, 10];
+let hat: [string, number, number?, ...number[]] = ["Hat", 100, 10, 1.20, 3, 0.95];
+let gloves: [string, number, number?, ...number[]] = ["Gloves", 75, 10];
 
 [hat, gloves].forEach(tuple => {
-    let [name, price, taxRate] = tuple;
+    let [name, price, taxRate, ...coupons] = tuple;
     if (taxRate != undefined) {
         price += price * (taxRate / 100);
     }
+    coupons.forEach(c => price -= c);
     writePrice(name, price);
 })
 
 console.log();
 
-let products: [string, number, number?][] = [["Hat", 100], ["Gloves", 75]];
-let tupleUnion: ([string, number, number?] | boolean)[] = [true, false, hat, ...products];
+let products: [string, number, number?, ...number[]][] = [["Hat", 100], ["Gloves", 75]];
+let tupleUnion: ([string, number, number?, ...number[]] | boolean)[] = [true, false, hat, ...products];
 tupleUnion.forEach((elem: [string, number] | boolean) => {
     if (elem instanceof Array) {
         let [str, num] = elem;
