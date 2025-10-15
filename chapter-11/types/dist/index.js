b7a340b5e8f07d93d42dbdb76dfbcc937c7b8072
Owain Williams <owain@hedfan.uk>
2025-10-15 18:15:26 +0100

    Make Person an abstract class with a getSpecificDetails abstract method

--- chapter-11/types/dist/index.js
+++ chapter-11/types/dist/index.js
@@ -7,6 +7,9 @@ class Person {
         this.name = name;
         this.city = city;
     }
+    getDetails() {
+        return `${this.name}, ${this.getSpecificDetails()}`;
+    }
 }
 ;
 class Employee extends Person {
@@ -21,8 +24,8 @@ class Employee extends Person {
         this.dept = dept;
         this.city = city;
     }
-    writeDept() {
-        console.log(`${this.name} works in ${this.dept}`);
+    getSpecificDetails() {
+        return `works in ${this.dept}`;
     }
 }
 ;
@@ -38,6 +41,9 @@ class Customer extends Person {
         this.city = city;
         this.creditLimit = creditLimit;
     }
+    getSpecificDetails() {
+        return `has ${this.creditLimit} limit`;
+    }
 }
 class Supplier extends Person {
     id;
@@ -51,21 +57,13 @@ class Supplier extends Person {
         this.city = city;
         this.companyName = companyName;
     }
+    getSpecificDetails() {
+        return `works for ${this.companyName}`;
+    }
 }
 let data = [
     new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
     new Customer("ajones", "Alice Jones", "London", 500)
 ];
 data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
-data.forEach(item => {
-    console.log(`Person: ${item.name}, ${item.city}`);
-    if (item instanceof Employee) {
-        item.writeDept();
-    }
-    else if (item instanceof Customer) {
-        console.log(`Customer ${item.name} has ${item.creditLimit} limit`);
-    }
-    else if (item instanceof Supplier) {
-        console.log(`Supplier ${item.name} works for ${item.companyName}`);
-    }
-});
+data.forEach(item => console.log(item.getDetails()));
