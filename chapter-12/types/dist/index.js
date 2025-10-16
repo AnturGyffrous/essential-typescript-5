16abb76c30f63b23bf1819ca7f300ca67b717461
Owain Williams <owain@hedfan.uk>
2025-10-16 17:30:56 +0100

    Add a PeopleCollection

--- chapter-12/types/dist/index.js
+++ chapter-12/types/dist/index.js
@@ -1,4 +1,22 @@
 import { Person, Product } from "./dataTypes.js";
 let people = [new Person("Bob Smith", "London"), new Person("Dora Peters", "New York")];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-[...people, ...products].forEach(item => console.log(`Item: ${item.name}`));
+class PeopleCollection {
+    items = [];
+    constructor(initialItems) {
+        this.items.push(...initialItems);
+    }
+    add(newItem) {
+        this.items.push(newItem);
+    }
+    getNames() {
+        return this.items.map(item => item.name);
+    }
+    getItem(index) {
+        return this.items[index];
+    }
+}
+let peopleData = new PeopleCollection(people);
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
