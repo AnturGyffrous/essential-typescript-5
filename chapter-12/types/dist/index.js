2fe012d4bcd247aa8367edc53f5e086803618c77
Owain Williams <owain@hedfan.uk>
2025-10-16 17:38:18 +0100

    Use a generic type parameter so DataCollection can use any type

--- chapter-12/types/dist/index.js
+++ chapter-12/types/dist/index.js
@@ -1,6 +1,7 @@
 import { Person, Product } from "./dataTypes.js";
 let people = [new Person("Bob Smith", "London"), new Person("Dora Peters", "New York")];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
+// type dataType = Person | Product;
 class DataCollection {
     items = [];
     constructor(initialItems) {
@@ -9,16 +10,16 @@ class DataCollection {
     add(newItem) {
         this.items.push(newItem);
     }
-    getNames() {
-        return this.items.map(item => item.name);
-    }
+    // getNames(): string[] {
+    //     return this.items.map(item => item.name);
+    // }
     getItem(index) {
         return this.items[index];
     }
 }
 let peopleData = new DataCollection(people);
-console.log(`Names: ${peopleData.getNames().join(", ")}`);
+// console.log(`Names: ${peopleData.getNames().join(", ")}`);
 let firstPerson = peopleData.getItem(0);
-if (firstPerson instanceof Person) {
-    console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-}
+// if (firstPerson instanceof Person) {
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+// }
