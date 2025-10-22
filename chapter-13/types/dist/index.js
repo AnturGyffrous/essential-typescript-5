95d6af12e510bba5bc6ac83f9fcb9e7c08a0c191
Owain Williams <owain@hedfan.uk>
2025-10-22 17:14:58 +0100

    Use mapped types to change property optionality and mutability
    A question mark (the ? character) is placed after the name selector to
    make the properties in the mapped type optional, and a minus sign and a
    question mark (the -? characters) are used to make properties required.
    Properties are made read-only and read-write by preceding the name
    selector with readonly and -readonly.
    
    Mapped types change all the properties defined by the type they
    transform so that the type produced by MakeOptional<T> when applied to
    the Product class, for example, is equivalent to this type:
    
    type optionalType = {
        name?: string;
        price?: number;
    }
    
    The types produced by mappings can be fed into other mappings, creating
    a chain of transformations. The type produced by the MakeOptional<T>
    mapping is then transformed by the MakeRequired<T> mapping, the output
    of which is then fed to the MakeReadOnly<T> mapping and then the
    MakeReadWrite<T> mapping. The result is that properties are made
    optional and then required and then read-only and, finally, read-write.

--- chapter-13/types/dist/index.js
+++ chapter-13/types/dist/index.js
@@ -1,8 +1,6 @@
 import { Product } from "./dataTypes.js";
 let p = { name: "Kayak", price: 275 };
 console.log(`Mapped type: ${p.name}, ${p.price}`);
-let c = { name: "London", population: 8136000 };
-console.log(`Mapped type: ${c.name}, ${c.population}`);
 console.log();
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 class Collection {
