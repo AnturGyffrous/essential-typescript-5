642feb46dcdb6c040cbedb3e1763bcd5d3115aef
Owain Williams <owain@hedfan.uk>
2025-10-22 16:12:24 +0100

    Constrain a generic type parameter using the keyof keyword
    The getValue function has a type parameter K that is constrained using
    typeof T, which means that K can be the name of only one of the
    properties defined by T, regardless of the type used for T when the
    function is invoked. When the getValue function is used with a Product
    object, the keyname parameter can be only name or price. And when the
    getValue function is used with an Employee object, the keyname parameter
    can be only name or role. In both cases, the keyname parameter can be
    used to safely get or set the value of the corresponding property from
    the Product or Employee object.

--- chapter-13/types/dist/index.js
+++ chapter-13/types/dist/index.js
@@ -1,7 +1,14 @@
-import { Product } from "./dataTypes.js";
-let myVar = "name";
-myVar = "price";
-myVar = "someOtherName";
+import { Product, Employee } from "./dataTypes.js";
+function getValue(item, keyname) {
+    console.log(`Value: ${item[keyname]}`);
+}
+let p = new Product("Running Shoes", 100);
+getValue(p, "name");
+getValue(p, "price");
+let e = new Employee("Bob Smith", "Sales");
+getValue(e, "name");
+getValue(e, "role");
+console.log();
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 class Collection {
     items;
