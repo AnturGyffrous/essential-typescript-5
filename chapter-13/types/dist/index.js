26681ebd367c6a55dad92350f009abf2ea201cb5
Owain Williams <owain@hedfan.uk>
2025-10-22 16:59:38 +0100

    Update the type mapping to change both the name and type of the properties
    The AllowStrings type is created with a mapping that creates a type
    union between string and the property's original type, like this:
    
    ...
    [P in keyof Product] : Product[P] | string
    ...
    
    The result is a type that is equivalent to this type:
    
    type AllowStrings = {
        name: string;
        price: number | string;
    }
    
    The ChangeNames type is created with a mapping that alters the name of
    each property by adding Property.
    
    ...
    [P in keyof Product as `${P}Property`] : Product[P]
    ...
    
    The as keyword is combined with an expression that defines the property
    name. In this case, a template string is used to modify the existing
    name, with the result that is equivalent to this type:
    
    type ChangeNames = {
        nameProperty: string;
        priceProperty: number;
    }

--- chapter-13/types/dist/index.js
+++ chapter-13/types/dist/index.js
@@ -1,6 +1,10 @@
 import { Product } from "./dataTypes.js";
 let p = { name: "Kayak", price: 275 };
 console.log(`Mapped type: ${p.name}, ${p.price}`);
+let q = { name: "Kayak", price: "apples" };
+console.log(`Changed type # 1: ${q.name}, ${q.price}`);
+let r = { nameProperty: "Kayak", priceProperty: 12 };
+console.log(`Changed type # 2: ${r.nameProperty}, ${r.priceProperty}`);
 console.log();
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 class Collection {
--- chapter-13/types/src/index.ts
+++ chapter-13/types/src/index.ts
@@ -7,6 +7,18 @@ type MappedProduct = {
 let p: MappedProduct = { name: "Kayak", price: 275 };
 console.log(`Mapped type: ${p.name}, ${p.price}`);
 
+type AllowStrings = {
+    [P in keyof Product]: Product[P] | string
+}
+let q: AllowStrings = { name: "Kayak", price: "apples" };
+console.log(`Changed type # 1: ${q.name}, ${q.price}`);
+
+type ChangeNames = {
+    [P in keyof Product as `${P}Property`]: Product[P]
+}
+let r: ChangeNames = { nameProperty: "Kayak", priceProperty: 12 };
+console.log(`Changed type # 2: ${r.nameProperty}, ${r.priceProperty}`);
+
 console.log();
 
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
