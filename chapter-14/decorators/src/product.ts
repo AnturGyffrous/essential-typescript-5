d5ff629eb80173ebdee5838ccd777fc61bc5f3e9
Owain Williams <owain@hedfan.uk>
2025-10-24 15:48:49 +0100

    Decorate the getDetails method of the Product class

--- chapter-14/decorators/src/product.ts
+++ chapter-14/decorators/src/product.ts
@@ -1,6 +1,9 @@
+import { time } from "./methodDecorator.js";
+
 export class Product {
     constructor(public name: string, public price: number) { }
 
+    @time
     getDetails(): string {
         return `Name: ${this.name}, Price $${this.price}`;
     }
