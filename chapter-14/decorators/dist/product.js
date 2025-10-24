a1603939da41cb1e6dd91ba548aff11335b7e49c
Owain Williams <owain@hedfan.uk>
2025-10-24 15:40:02 +0100

    Start the TypeScript compiler
    npm start

---
+++ chapter-14/decorators/dist/product.js
@@ -0,0 +1,11 @@
+export class Product {
+    name;
+    price;
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    getDetails() {
+        return `Name: ${this.name}, Price $${this.price}`;
+    }
+}
