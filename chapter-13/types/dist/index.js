39d51ebf934ec1e9fe2d0f77b7ac2f0c2de95819
Owain Williams <owain@hedfan.uk>
2025-10-24 14:16:12 +0100

    Use a conditional type in a type mapping

--- chapter-13/types/dist/index.js
+++ chapter-13/types/dist/index.js
@@ -49,3 +49,8 @@ function isProductOrCity(item) {
 }
 let filteredData = FilterArray(dataArray, isProductOrCity);
 filteredData.forEach(item => console.log(`Person: ${item.name}`));
+function convertProduct(p) {
+    return { name: p.name, price: `$${p.price.toFixed(2)}` };
+}
+let kayak = convertProduct(new Product("Kayak", 275));
+console.log(`Product: ${kayak.name}, ${kayak.price}`);
--- chapter-13/types/src/index.ts
+++ chapter-13/types/src/index.ts
@@ -83,3 +83,16 @@ function isProductOrCity(item: any): item is Product | City {
 
 let filteredData: Person[] = FilterArray(dataArray, isProductOrCity);
 filteredData.forEach(item => console.log(`Person: ${item.name}`));
+
+type changeProps<T, U, V> = {
+    [P in keyof T]: T[P] extends U ? V : T[P]
+};
+
+type modifiedProduct = changeProps<Product, number, string>;
+
+function convertProduct(p: Product): modifiedProduct {
+    return { name: p.name, price: `$${p.price.toFixed(2)}` };
+}
+
+let kayak = convertProduct(new Product("Kayak", 275));
+console.log(`Product: ${kayak.name}, ${kayak.price}`);
