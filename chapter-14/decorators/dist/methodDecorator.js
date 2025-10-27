6776645a1f40e73d214a748c7fa65f9481c7ad7d
Owain Williams <owain@hedfan.uk>
2025-10-27 13:13:24 +0000

    Pass an additional argument by nesting the existing time decorator in another function
    There are now three nested functions. The outer function is the factory
    responsible for receiving an optional string and returns the original
    decorator function. The decorator function receives the original method
    and the context object and is responsible for returning the replacement
    method.

--- chapter-14/decorators/dist/methodDecorator.js
+++ chapter-14/decorators/dist/methodDecorator.js
@@ -1,14 +1,13 @@
-// interface HasGetPrice {
-//     getPrice(): number;
-// }
-export function time(method, ctx) {
-    const methodName = String(ctx.name);
-    return function (...args) {
-        const start = performance.now();
-        console.log(`${methodName} started`);
-        const result = method.call(this, ...args);
-        const duration = (performance.now() - start).toFixed(2);
-        console.log(`${methodName} ended ${duration} ms`);
-        return result;
+export function time(label) {
+    return function (method, ctx) {
+        const methodName = label ?? String(ctx.name);
+        return function (...args) {
+            const start = performance.now();
+            console.log(`${methodName} started`);
+            const result = method.call(this, ...args);
+            const duration = (performance.now() - start).toFixed(2);
+            console.log(`${methodName} ended ${duration} ms`);
+            return result;
+        };
     };
 }
--- chapter-14/decorators/src/methodDecorator.ts
+++ chapter-14/decorators/src/methodDecorator.ts
@@ -1,17 +1,17 @@
-// interface HasGetPrice {
-//     getPrice(): number;
-// }
+export function time(label?: string) {
+    return function <This, Args extends any[], Result extends string | number>(
+        method: (This, Args) => Result,
+        ctx: ClassMethodDecoratorContext<This, (This, Args) => Result>) {
 
-export function time<This, Args extends any[], Result extends string | number>(
-    method: (This, Args) => Result,
-    ctx: ClassMethodDecoratorContext<This, (This, Args) => Result>) {
-    const methodName = String(ctx.name);
-    return function (this: This, ...args: Args) {
-        const start = performance.now();
-        console.log(`${methodName} started`);
-        const result = method.call(this, ...args);
-        const duration = (performance.now() - start).toFixed(2);
-        console.log(`${methodName} ended ${duration} ms`);
-        return result;
+        const methodName = label ?? String(ctx.name);
+
+        return function (this: This, ...args: Args) {
+            const start = performance.now();
+            console.log(`${methodName} started`);
+            const result = method.call(this, ...args);
+            const duration = (performance.now() - start).toFixed(2);
+            console.log(`${methodName} ended ${duration} ms`);
+            return result;
+        }
     }
 }
