70b635310c363eddb35217d436bb40abd79fc37e
Owain Williams <owain@hedfan.uk>
2025-10-27 16:19:56 +0000

    Pass a string to the costFormatter
    Note that TypeScript compiles without an error, but the code throws a
    runtime error because it doesn't check the value passed is a number
    before calling toFixed.

--- chapter-15/usingjs/dist/index.js
+++ chapter-15/usingjs/dist/index.js
@@ -9,4 +9,4 @@ cart.addProduct(kayak, 1);
 cart.addProduct(hat, 1);
 cart.addProduct(hat, 2);
 sizeFormatter("Cart", cart.itemCount);
-costFormatter("Cart", cart.totalPrice);
+costFormatter("Cart", `${cart.totalPrice}`);
