4dbae1dda4fd8f38d80299a1d6b89e1a07fb675d
Owain Williams <owain@hedfan.uk>
2025-10-27 13:30:15 +0000

    Add a call to getPrice so the effect of both decorators is shown

--- chapter-14/decorators/dist/index.js
+++ chapter-14/decorators/dist/index.js
@@ -5,6 +5,7 @@ let city = new City("London", 8982000);
 let product = new Product("Kayak", 275);
 console.log(city.getSummary());
 console.log(product.getDetails());
+console.log(`Price: ${product.getPrice()}`);
 console.log(`Get Product tax: ${product.tax}`);
 product.tax = 30;
 if (isSerializeable(product)) {
