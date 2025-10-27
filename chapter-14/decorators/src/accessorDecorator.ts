ed3239365fd87f9877a858769ab1ea501bb81a5d
Owain Williams <owain@hedfan.uk>
2025-10-27 12:02:31 +0000

    Use generic type parameters to constrain the decorator to number accessors

--- chapter-14/decorators/src/accessorDecorator.ts
+++ chapter-14/decorators/src/accessorDecorator.ts
@@ -1,4 +1,6 @@
-export function log(accessor: any, ctx: ClassSetterDecoratorContext | ClassGetterDecoratorContext) {
+export function log<This, ValueType extends number>(setter: (ValueType) => void, ctx: ClassSetterDecoratorContext<This, ValueType>): ((ValueType) => void);
+export function log<This, ValueType extends number>(getter: () => ValueType, ctx: ClassGetterDecoratorContext<This, ValueType>): () => ValueType;
+export function log(accessor: any, ctx: any) {
     const name = String(ctx.name);
 
     return function (this: any, ...args: any[]) {
