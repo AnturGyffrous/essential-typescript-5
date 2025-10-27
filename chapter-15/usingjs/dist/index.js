ae3d2c5c035db67dacfa4c64a0ae2e09ee869515
Owain Williams <owain@hedfan.uk>
2025-10-27 16:06:16 +0000

    Use the JavaScript formatting functions in index.ts

--- chapter-15/usingjs/dist/index.js
+++ chapter-15/usingjs/dist/index.js
@@ -1,5 +1,6 @@
 import { SportsProduct, SPORT } from "./product.js";
 import { Cart } from "./cart.js";
+import { sizeFormatter, costFormatter } from "./formatters.js";
 let kayak = new SportsProduct(1, "Kayak", 275, SPORT.Watersports);
 let hat = new SportsProduct(2, "Hat", 22.10, SPORT.Running, SPORT.Watersports);
 let ball = new SportsProduct(3, "Soccer Ball", 19.50, SPORT.Soccer);
@@ -7,5 +8,5 @@ let cart = new Cart("Bob");
 cart.addProduct(kayak, 1);
 cart.addProduct(hat, 1);
 cart.addProduct(hat, 2);
-console.log(`Cart has ${cart.itemCount} items`);
-console.log(`Cart value is $${cart.totalPrice.toFixed(2)}`);
+sizeFormatter("Cart", cart.itemCount);
+costFormatter("Cart", cart.totalPrice);
