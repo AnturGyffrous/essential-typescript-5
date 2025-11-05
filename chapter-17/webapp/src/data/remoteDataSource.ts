eadf2976dbe71648c7cd51c95809140fa2891d81
Owain Williams <owain@hedfan.uk>
2025-11-05 21:19:04 +0000

    Update the remoteDataSource.ts to use relative URLs

--- chapter-17/webapp/src/data/remoteDataSource.ts
+++ chapter-17/webapp/src/data/remoteDataSource.ts
@@ -2,19 +2,16 @@ import { AbstractDataSource } from "./abstractDataSource";
 import { Product, Order } from "./entities";
 import Axios from "axios";
 
-const protocol = "http";
-const hostname = "localhost";
-const port = 4600;
-
 const urls = {
-    products: `${protocol}://${hostname}:${port}/products`,
-    orders: `${protocol}://${hostname}:${port}/orders`
+    products: "/api/products",
+    orders: "/api/orders"
 };
 
 export class RemoteDataSource extends AbstractDataSource {
     loadProducts(): Promise<Product[]> {
         return Axios.get(urls.products).then(response => response.data);
     }
+
     storeOrder(): Promise<number> {
         let orderData = {
             lines: [...this.order.orderLines.values()].map(ol => ({
@@ -23,6 +20,7 @@ export class RemoteDataSource extends AbstractDataSource {
                 quantity: ol.quantity
             }))
         }
+
         return Axios.post(urls.orders, orderData)
             .then(response => response.data.id);
     }
