5a8b211455ecc85d41d0cde54ddfdc35b4178d54
Owain Williams <owain@hedfan.uk>
2025-11-11 14:48:23 +0000

    Copy the reactapp directory from chapter-20 to chapter-21
    rsync -av --progress chapter-20/reactapp ./chapter-21 --exclude node_modules
    cd chapter-21/reactapp
    npm install

---
+++ chapter-21/reactapp/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import { Product } from './data/entities';
+import { ProductList } from './productList';
+import { useAppDispatch, useAppSelector, reducers, queries } from './data/dataStore';
+
+export const App: FunctionComponent = () => {
+
+  const selections = useAppSelector(state => state.selections);
+  const dispatch = useAppDispatch();
+
+  const { data } = queries.useGetProductsQuery();
+
+  const addToOrder = (product: Product, quantity: number) =>
+    dispatch(reducers.addToOrder([product, quantity]));
+
+  const categories = useMemo<string[]>(() => {
+    return [...new Set(data?.map(p => p.category))]
+  }, [data]);
+
+  return <div className="App">
+    <ProductList products={data ?? []}
+      categories={categories}
+      selections={selections}
+      addToOrder={addToOrder} />
+  </div>
+}
+
+export default App;
